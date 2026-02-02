79c45530ce160f1b16f72bd2c3b4db9e964ba22a
v0rvex <govnoed830@gmail.com>
2026-02-03 02:12:03 +0500

    binding changed

--- studyline-front/src/api/client.ts
+++ studyline-front/src/api/client.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API_BASE = (import.meta.env.VITE_API_BASE as string) || 'http://localhost:8080'
+const API_BASE = (import.meta.env.VITE_API_BASE as string) || 'http://0.0.0.0:1200'
 const api = axios.create({ baseURL: API_BASE, withCredentials: true, headers: { 'Content-Type': 'application/json' } })
 api.interceptors.response.use(r=>r, err=>{
   const ev = new CustomEvent('app:error', { detail: err })
